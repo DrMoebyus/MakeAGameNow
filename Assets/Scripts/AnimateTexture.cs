7c1ddc81ad31d37d9a49cbb67bcccd697b135271
DrMoebyus <moebyus@gmx.at>
2017-02-10 06:13:46 +0100

    Animated texture "movement"

---
+++ Assets/Scripts/AnimateTexture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateTexture : MonoBehaviour
+{
+    public Vector2 speed = Vector2.one;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        GetComponent<Renderer>().material.mainTextureOffset += speed * Time.deltaTime;
+    }
+}

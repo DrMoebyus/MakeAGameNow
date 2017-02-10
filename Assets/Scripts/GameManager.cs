1d0e586b4646d3bdff148ca0877c460fc92ea98c
DrMoebyus <moebyus@gmx.at>
2017-02-10 10:53:43 +0100

    Score system and unified speed system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,36 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
-    private bool gameOver = false;
+public class GameManager : MonoBehaviour {
 
-	void Start ()
-	{
-		
+	public static GameManager Instance;
+
+	public float pointsPerUnitTraveld = 1.0f;
+	public float gameSpeed = 10.0f;
+
+	private float score = 0.0f;
+	private bool gameOver = false;
+	private static float highScore = 0.0f;
+
+	void Start () {
+		Instance = this;
 	}
 
-	void Update ()
-	{
-		if (GameObject.FindGameObjectWithTag("Player") == null)
-        {
-            gameOver = true;
-        }
+	void Update () {
+		if (GameObject.FindGameObjectWithTag("Player") == null)	{
+			gameOver = true;
+		}
 
-        if (gameOver)
-        {
-            if (Input.anyKeyDown)
-            {
-                SceneManager.LoadScene("Level1");
-            }
-        }
+		if (gameOver)	{
+			if (Input.anyKeyDown) {
+				SceneManager.LoadScene("Level1");
+			}
+		}
+
+		if (!gameOver) {
+			score += pointsPerUnitTraveld * gameSpeed * Time.deltaTime;
+
+			if (score > highScore) {
+					highScore = score;
+			}
+		}
 	}
 
-    void OnGUI ()
-    {
-        if (gameOver == true)
-        {
-            GUILayout.Label("Game Over!  Press any key to reset!");
-        }
-    }
+	void OnGUI () {
+		int currentScore = (int)score;
+		int currentHighScore = (int)highScore;
+		GUILayout.Label("Score: " + currentScore.ToString());
+		GUILayout.Label("highScore: " + currentHighScore.ToString());
+
+		if (gameOver == true) {
+			GUILayout.Label("Game Over!  Press any key to reset!");
+		}
+	}
 }
---
+++ Assets/Scripts/MoveBasedOnGameSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBasedOnGameSpeed : MonoBehaviour
+{
+    public Vector3 direction = Vector3.forward;
+
+	void Update ()
+    {
+        transform.position += transform.rotation*(direction.normalized*GameManager.Instance.gameSpeed*Time.deltaTime);
+    }
+}

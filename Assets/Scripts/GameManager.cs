c6e4201fdf6b52fad0b59521a6faf7998b7fbd8f
DrMoebyus <moebyus@gmx.at>
2017-02-10 08:54:30 +0100

    It's a game!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameOver = false;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		if (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            gameOver = true;
+        }
+
+        if (gameOver)
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene("Level1");
+            }
+        }
+	}
+
+    void OnGUI ()
+    {
+        if (gameOver == true)
+        {
+            GUILayout.Label("Game Over!  Press any key to reset!");
+        }
+    }
+}
